b15abdb8815a68b487033a04fb2dc2ee0e9c6ee5
Candra Kupang <2311218k@student.gla.ac.uk>
2018-01-21 14:48:52 +0000

    Chapter 3

--- rango/urls.py
+++ rango/urls.py
@@ -11,15 +11,13 @@ Class-based views
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls')) 
 """
+
 from django.conf.urls import url
-from django.contrib import admin
-from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^rango/', include('rango.urls')),
-    url(r'^admin/', admin.site.urls),
+	url(r'^about/', views.about, name='about'),
 ]
