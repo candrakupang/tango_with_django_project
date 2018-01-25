d971fbdb78584c77a9ba47447b2df9fbd37f53f6
Candra Kupang <2311218k@student.gla.ac.uk>
2018-01-25 09:52:25 +0000

    Chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -82,6 +82,8 @@ MEDIA_ROOT = MEDIA_DIR
 
 MEDIA_URL = '/media/'
 
+LOGIN_URL = '/rango/login/'
+
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 
@@ -95,6 +97,11 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = (
+	'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+	'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -105,6 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+		'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
